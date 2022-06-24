a2c61933337557e0ac6f5b5ab2299bd18f72aca5
서성경 <sk@seoseong-gyeong-ui-Macmini.local>
2022-06-24 15:24:53 +0900

    move private information into dev.js

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://koreasledo:tjdrudTJ915!@react-blog.10ao1.mongodb.net/?retryWrites=true&w=majority', 
+const bodyPerser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const config = require('./config/key,');
+
+const { User } = require('./models/user');
+
+
+mongoose.connect(config.mongoURI, 
     { useNewUrlParser: true }).then(() => console.log('DB connected'))
                                 .catch(err => console.log(err));
 
-app.get('/', (req,res) => {
-    res.send('hello world')
-});
+app.use(bodyPerser.urlencoded({ extended : true }));
+app.use(bodyPerser.json());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.json({"hello~": "Hi~~ sfsfsfg"})
+})
+
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body)
+    user.save((err, userData) => {
+        if(err) return res.json({ success: false, err })
+
+        return res.status(200).json({
+            success:true
+        })
+    })
+})
 
 app.listen(5000);
